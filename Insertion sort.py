061b343ac544879c685cc94836bde6f4dceb3820
Muzammil Ahmed <muzidxb1@gmail.com>
2021-09-30 19:26:29 +0000

    Create Insertion sort.py
    A sort method which places an unsorted element at its suitable place in each iteration.

---
+++ Insertion sort.py
@@ -0,0 +1,8 @@
+def insertionSort(a):
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and key < a[j]:
+            a[j + 1] = a[j]
+            j = j - 1
+        a[j + 1] = key
