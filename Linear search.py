0c6a03aa392c5720ac5d1b75e4d87bce4cc6ac00
Muzammil Ahmed <muzidxb1@gmail.com>
2021-10-02 11:03:57 +0000

    Update Linear search.py

--- Linear search.py
+++ Linear search.py
@@ -1,8 +1,8 @@
-# Take an example list and key to search
-a = [10, 20, 3, 44, 5, -6]
+# Input list and item to search (key)
+a = [int(x) for x in input('Enter elements seperated by space: ').split()]
 key = int(input('Enter element to search: '))
 
-# Using For loop
+# Then search one by one using for loop
 for i in range(len(a)):  
     if a[i] == key:
         print('key is at element', i + 1) # +1 because loop starts at 0
@@ -10,6 +10,12 @@ for i in range(len(a)):
 else:
     print(key, 'not available')
 
+    
+    
+    
+    
+    
+    
 # Using While loop
 i = 0
 while i < len(a):
