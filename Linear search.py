fa5b7ee3a5fd9fa62a20f63a7511163ff357d1eb
Muzammil Ahmed <muzidxb1@gmail.com>
2021-10-05 19:13:13 +0000

    Using function

--- Linear search.py
+++ Linear search.py
@@ -1,11 +1,19 @@
-# Input list and item to search (key)
-a = [int(x) for x in input('Enter elements separated by space: ').split()]
-key = int(input('Enter element to search: '))
+def linsearch(array, key):
+    """Return string whether key is found in list"""
+    for i in range(len(array)):
+        if array[i] == key:
+            print('Found at position', i + 1)
+            break
+    else:
+        print('Not found')
 
-# Then search one by one using for loop
-for i in range(len(a)):
-    if a[i] == key:
-        print('key is at element', i + 1)  # +1 because loop starts at 0
-        break
-else:
-    print(key, 'not available')
+array = [] # empty list to append values
+length = int(input('Enter length of list ')) # to determine index of loop
+
+# append values using for-loop
+for i in range(length):
+    temp = input('Enter next value ')
+    array.append(temp)
+
+key = input('Enter key to search ')
+linsearch(array, key)
