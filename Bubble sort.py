245d898e8fc8841c029454ac4c6989879d1bac05
Muzammil Ahmed <muzidxb1@gmail.com>
2021-10-06 13:14:08 +0000

    Update Bubble sort.py

--- Bubble sort.py
+++ Bubble sort.py
@@ -1,16 +1,16 @@
 def bubsort(arr):
-    for i in range(len(arr)-1, 0, -1): # reverse order, length to 0
+    for i in range(len(arr)-1, 0, -1): # loop from length to 0 (reverse)
         for j in range(i): # again loop for multiple passes
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j] # Swap
   # arr[::-1] here for descending order
 
-array = [] # empty list to append values
+arr = [] # empty list to append values
 length = int(input('Enter length of list ')) # to determine index of loop
 
 # append values using for-loop
 for i in range(length):
     temp = input('Enter value ')
-    array.append(temp)
+    arr.append(temp)
 
-bubsort(array)
+bubsort(arr)
