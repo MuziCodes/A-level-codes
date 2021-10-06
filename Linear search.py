9bb9a2981a54b54b79b4a8779dfd5324c25bee61
Muzammil Ahmed <muzidxb1@gmail.com>
2021-10-06 13:12:37 +0000

    Update Linear search.py

--- Linear search.py
+++ Linear search.py
@@ -1,18 +1,18 @@
-def linsearch(array, key):
-    for i in range(len(array)):
-        if array[i] == key:
+def linsearch(arr, key):
+    for i in range(len(arr)):
+        if arr[i] == key:
             print('Found at position', i + 1)
             break
     else:
         print('Not found')
 
-array = [] # empty list to append values
+arr = [] # empty list to append values
 length = int(input('Enter length of list ')) # to determine index of loop
 
 # append values using for-loop
 for i in range(length):
     temp = input('Enter value ')
-    array.append(temp)
+    arr.append(temp)
 
 key = input('Enter key to search ')
-linsearch(array, key)
+linsearch(arr, key)
