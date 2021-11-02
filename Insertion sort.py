3b4214806f6f5683e92f5a96ce59513be73ade9d
Muzammil Ahmed <muzidxb1@gmail.com>
2021-11-02 17:13:32 +0000

    Update Insertion sort.py

--- Insertion sort.py
+++ Insertion sort.py
@@ -4,14 +4,4 @@ def insertsort(arr):
         while arr[i - 1] > pivot and i > 0: # if left item larger, swap & use only positive index
             arr[i], arr[i-1] = arr[i-1], arr[i] # swap
             i = i-1 # decrement
-
-arr = [] # empty list to append values
-length = int(input('Enter length of list ')) # to determine index of loop
-
-# append values using for-loop
-for i in range(length):
-    temp = int(input('Enter value '))
-    arr.append(temp)
-
-insertsort(arr)
-print(arr)
+            
