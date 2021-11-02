8f692ba140a41b3e2c1dc5f940b9f7ba7e4a3351
Muzammil Ahmed <muzidxb1@gmail.com>
2021-11-02 17:13:12 +0000

    Update Bubble sort.py

--- Bubble sort.py
+++ Bubble sort.py
@@ -4,14 +4,3 @@ def bubsort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j] # Swap
   # arr[::-1] here for descending order
-
-arr = [] # empty list to append values
-length = int(input('Enter length of list ')) # to determine index of loop
-
-# append values using for-loop
-for i in range(length):
-    temp = int(input('Enter value '))
-    arr.append(temp)
-
-bubsort(arr)
-print(arr)
