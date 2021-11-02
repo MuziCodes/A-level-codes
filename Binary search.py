bca7bb8d781ad6297d0b92381556dc39de6798b6
Muzammil Ahmed <muzidxb1@gmail.com>
2021-11-02 17:12:36 +0000

    Update Binary search.py

--- Binary search.py
+++ Binary search.py
@@ -14,15 +14,3 @@ def binsearch(arr, key):
             upB = mid - 1
     else:
         print('Not found')
-
-arr = [] # empty list to append values
-length = int(input('Enter length of list ')) # to determine index of loop
-
-# append values using for-loop
-for i in range(length):
-    temp = int(input('Enter value '))
-    arr.append(temp)
-
-key = input('Enter key to search ')
-binsearch(arr, key)
-print(arr)
