cb48d63fe732da2555c60eb7bd8d52223087a0a0
Muzammil Ahmed <muzidxb1@gmail.com>
2021-11-02 17:15:12 +0000

    Create Input array.py

---
+++ Input array.py
@@ -0,0 +1,7 @@
+arr = [] # empty list to append values
+length = int(input('Enter length of list: ')) # to determine index of loop
+
+# append values using for-loop
+for i in range(length):
+    temp = int(input('Enter value to add: '))
+    arr.append(temp)
