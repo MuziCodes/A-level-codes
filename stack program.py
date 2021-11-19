86d16549fcd0be57cbeb33ae708fe5ff3569656c
Muzammil Ahmed <muzidxb1@gmail.com>
2021-11-19 18:31:44 +0000

    Update and rename stack program 2.py to stack program.py

--- stack program.py
+++ stack program.py
@@ -1,4 +1,4 @@
-size = int(input('Enter size of stack: '))
+size = int(input("Enter size of stack: "))
 stack = [None for index in range(size)]  # make empty stack with size of input
 base = 0
 top = -1  # so 1st element will be index 0 after top += 1 below
@@ -14,6 +14,13 @@ def push(item):
         print("Overflow! cannot push.", stack)
 
 
+# ALTERNATIVE:
+# if len(stack) == size:
+#     print("Overflow! cannot push.", stack)
+# else:
+#     stack.append(item)
+
+
 def pop():
     global top, base, item
     if top == base - 1:  # basically if stack empty
@@ -21,19 +28,27 @@ def pop():
     else:
         item = stack[top]
         stack[top] = None  # replace empty element with None for display
-        top = top-1
+        top = top - 1
         print(item, "is removed")
 
 
+# ALTERNATIVE:
+# if len(stack) == 0:
+#   print("Underflow! cannot pop.")
+# else:
+#   stack = stack[:-1]
+
+
 while True:  # runs infinitely until break
-    option = int(input('''Choose an option:
+    option = int(input("""Choose an option:
 (1) Push
 (2) Pop
 (3) Display stack
 (4) Exit
-→ '''))
+→ """))
+
     if option == 1:
-        item = input('Enter item to push: ')
+        item = input("Enter item to push: ")
         push(item)
     elif option == 2:
         pop()
@@ -42,4 +57,4 @@ while True:  # runs infinitely until break
     elif option == 4:
         break
     else:
-        print('Please enter a number between 1-4')
+        print("Please enter a number between 1-4")
