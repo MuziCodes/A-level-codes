f5c73cea9cebef6fff5738b59e55f6fb8728a693
Muzammil Ahmed <muzidxb1@gmail.com>
2022-01-09 14:18:48 +0000

    Update Bubble sort.py

--- Bubble sort.py
+++ Bubble sort.py
@@ -3,4 +3,4 @@ def bubsort(arr):
         for j in range(len(arr)-i-1):  # ignore sorted so -i-1
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]  # Swap
-    # arr[::-1] here for descending order
+    # arr[:] = arr[::-1] here for descending order
