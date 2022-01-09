7f474ea82ba1d87f15c32425815971db901e626e
Muzammil Ahmed <muzidxb1@gmail.com>
2022-01-09 14:21:19 +0000

    Update Input array.py

--- Input array.py
+++ Input array.py
@@ -3,7 +3,7 @@ length = int(input('Enter length of list: '))
 # append values to empty list using for-loop
 for i in range(length):
     temp = int(input('Enter element to add: '))
-    arr.append(temp)  # arr[length:length] = temp appends using slicing
+    arr.append(temp)  # alternative: arr[length:length] = temp appends using slicing
 
 # ALTERNATIVE:
 # arr = [int(i) for i in input('Enter elements seperated by spaces: ').split()]
