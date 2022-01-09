094633980ae30126a12143bca609de6ff181e934
Muzammil Ahmed <muzidxb1@gmail.com>
2022-01-09 17:15:30 +0000

    Update Insertion sort.py

--- Insertion sort.py
+++ Insertion sort.py
@@ -2,6 +2,6 @@ def insertsort(arr):
     for i in range(1, len(arr)):
         pointer = arr[i]
         while arr[i - 1] > pointer and i > 0: # if left item larger & positive index
-            arr[i], arr[i - 1] = arr[i - 1], arr[i] # swap
-            i = i - 1 # decrement
+            arr[i - 1], arr[i] = arr[i], arr[i - 1] # swap
+            i -= 1 # decrement
     # arr[:] = arr[::-1] here for descending
