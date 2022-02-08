748b3d7a4e70b32bff965c18fc417b2953751df7
Muzammil Ahmed <muzidxb1@gmail.com>
2022-02-08 09:55:20 +0400

    Update Binary Tree_Hodder's.py

--- Binary Tree_Hodder's.py
+++ Binary Tree_Hodder's.py
@@ -28,3 +28,17 @@ class node:
             if self.item is None:  # if item not found unil leaf node reached
                 return None
         return self.item
+
+    
+#     FUNCTION FindElement(Item : INTEGER) RETURNS INTEGER 
+#  CurrentPointer ← RootPointer 
+#  WHILE CurrentPointer <> NullPointer  
+#   IF List[CurrentPointer].Data <> Item 
+#    THEN 
+#     CurrentPointer ← List[CurrentPointer].Pointer 
+#    ELSE 
+#     RETURN CurrentPointer 
+#   ENDIF 
+#  ENDWHILE 
+#  CurrentPointer ← NullPointer 
+#  RETURN CurrentPointer 
