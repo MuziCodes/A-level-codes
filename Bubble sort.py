958e94c133a9abee3df1d1e19089fc3ffbba8510
Muzammil Ahmed <muzidxb1@gmail.com>
2022-03-05 21:02:40 +0000

    Update Bubble sort.py

--- Bubble sort.py
+++ Bubble sort.py
@@ -4,3 +4,6 @@ def bubsort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]  # Swap
     # arr[:] = arr[::-1] here for descending order
+
+# Has order of growth/time complexity as O(n²) but O(n) if list already sorted
+# Goes through all elements and swaps by comparing with next value for each pass
