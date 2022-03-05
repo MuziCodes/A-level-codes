ef567edbb63f9c789a133386b7962302a074ff36
Muzammil Ahmed <muzidxb1@gmail.com>
2022-03-05 21:05:09 +0000

    Update Linear search.py

--- Linear search.py
+++ Linear search.py
@@ -5,3 +5,8 @@ def linsearch(arr, key):
             break
     else:  # ← BEWARE of indentation
         print('Not found')
+
+# Searches through all elements in list until value is found
+# This means if item is at the last value, it will search the whole list.
+# Binary search is a better alternative as it skips half the list.
+# Order of growth/time complexity O(n) in Big O notation
