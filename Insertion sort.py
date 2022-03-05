a5bf73c15430054e40b2b03e9a58f4c024307a3b
Muzammil Ahmed <muzidxb1@gmail.com>
2022-03-05 21:03:24 +0000

    Update Insertion sort.py

--- Insertion sort.py
+++ Insertion sort.py
@@ -5,3 +5,9 @@ def insertsort(arr):
             arr[i - 1], arr[i] = arr[i], arr[i - 1] # swap
             i -= 1 # decrement
     # arr[:] = arr[::-1] here for descending
+
+# Has order of growth/time complexity as O(n²)
+# Performance depends on size of array and how sorted they are.
+# Especially more efficient when compared to bubble sort as
+# When size is large, less swaps are made
+# And when it's almost sorted, it will stop as soon as it's sorted
