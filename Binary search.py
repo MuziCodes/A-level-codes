a53f0da292eef7611810226c6d36c0e8fb6219a9
Muzammil Ahmed <muzidxb1@gmail.com>
2022-03-05 21:05:27 +0000

    Update Binary search.py

--- Binary search.py
+++ Binary search.py
@@ -1,8 +1,6 @@
 def binsearch(arr, key):
-    """Binary search works on sorted list"""
     lowB = 0       # lower bound
     upB = len(arr) - 1 # upper bound
-    
     while lowB <= upB:
         mid = (lowB + upB) // 2 # int of mid value
         if arr[mid] == key:
@@ -14,3 +12,9 @@ def binsearch(arr, key):
             upB = mid - 1
     else:  # ← BEWARE of indentation
         print('Not found')
+
+# repeated checking of the middle item in an ordered search list and discarding the half
+# of the list which does not contain the search item
+# Order of growth/time complexity of O(Log₂ n) in Big O notation
+# Has to be sorted before usage as the midpoint is the middle element not the average value
+# it might even discard the value to search as the higher values will not be higher in reality.
