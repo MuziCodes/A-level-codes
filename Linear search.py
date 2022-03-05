0cc3a8317d1f49c76b7079e045d3ec700652e752
Muzammil Ahmed <muzidxb1@gmail.com>
2022-03-05 21:24:49 +0000

    Update Linear search.py

--- Linear search.py
+++ Linear search.py
@@ -6,6 +6,25 @@ def linsearch(arr, key):
     else:  # ← BEWARE of indentation
         print('Not found')
 
+# ///PSEUDOCODE\\\
+
+INPUT key
+Index ← 0
+Found ← FALSE
+REPEAT // or FOR Index 1 to LENGTH(list)
+    IF list[index] = key
+      THEN
+        Found ← TRUE
+        OUTPUT "Value found at " Index // or RETURN Index if function
+    ENDIF
+    Index ← Index + 1
+UNTIL Index = LENGTH(list)
+
+IF Found = FALSE
+    THEN
+      OUTPUT "Not Found"
+ENDIF
+
 # Searches through all elements in list until value is found
 # This means if item is at the last value, it will search the whole list.
 # Binary search is a better alternative as it skips half the list.
