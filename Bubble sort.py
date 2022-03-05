514d696d8e40af0c56d92b24a08c4e7c041830f8
Muzammil Ahmed <muzidxb1@gmail.com>
2022-03-05 21:33:49 +0000

    Update Bubble sort.py

--- Bubble sort.py
+++ Bubble sort.py
@@ -5,5 +5,23 @@ def bubsort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]  # Swap
     # arr[:] = arr[::-1] here for descending order
 
+# ///PSEUDOCODE\\\
+
+# n ← MaxIndex - 1
+FOR i ← 0 TO MaxIndex - 1
+  FOR j ← 0 TO n
+    IF List[j] > List[j + 1]
+      THEN
+        Temp ← List[j]
+        List[j] ← List[j + 1]
+        List[j + 1] ← Temp
+    ENDIF
+  ENDFOR
+  n ← n - 1
+ENDFOR
+
+# this code is inefficient as iterations continue even after array is sorted
+# to enhance, use a flag to indicate if any swaps have taken place.
+# if inner loop has made all comparisions with no changes, set flag to true
 # Has order of growth/time complexity as O(n²) but O(n) if list already sorted
 # Goes through all elements and swaps by comparing with next value for each pass
