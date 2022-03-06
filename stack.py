fb3a025b1691fafdc30ad627693b0344b1bb166e
Muzammil Ahmed <muzidxb1@gmail.com>
2022-03-06 17:57:29 +0000

    Update stack.py

--- stack.py
+++ stack.py
@@ -3,11 +3,11 @@ stack = [None for i in range(size)]  # make empty stack with size of input
 base = 0
 top = -1  # so 1st element will be index 0 after top += 1 below
 
-def push(item):
+def push(value):
     global top  # global variable to affect outside function
     if top < size - 1:  # -1 since 1st index is 0, basically if top not max..
         top += 1
-        stack[top] = item
+        stack[top] = value
     else:
         print("Overflow! cannot push.")
 
@@ -22,32 +22,31 @@ def pop():
 
 # ///PSEUDOCODE\\\
 
-# CONSTANT EMPTYSTRING = ""
-# CONSTANT NullPointer = –1
-# CONSTANT MaxStackSize = 8
-# DECLARE BaseOfStackPointer : INTEGER
-# DECLARE TopOfStackPointer : INTEGER
-# DECLARE Stack : ARRAY[1 : MaxStackSize – 1] OF STRING
+# CONSTANT nullp = –1
+# CONSTANT size = 8
+# DECLARE base : INTEGER
+# DECLARE top : INTEGER
+# DECLARE Stack : ARRAY[1 : size – 1] OF STRING
 # PROCEDURE InitialiseStack
-#     BaseOfStackPointer ← 0
-#     TopOfStackPointer ← NullPointer
+#     base ← 0
+#     top ← nullp
 # ENDPROCEDURE
 
-# PROCEDURE Push(NewItem)
-#     IF TopOfStackPointer < MaxStackSize – 1
+# PROCEDURE Push(value)
+#     IF top < size – 1
 #       THEN
-#         TopOfStackPointer ← TopOfStackPointer + 1
-#         Stack[TopOfStackPointer] ← NewItem
+#         top ← top + 1
+#         Stack[top] ← value
 #     ENDIF
 # ENDPROCEDURE
 
 # FUNCTION Pop()
 #     DECLARE Item : STRING
-#     Item ← EMPTYSTRING
-#     IF TopOfStackPointer > NullPointer
+#     Item ← ""
+#     IF top > nullp
 #       THEN
-#         Item ← Stack[TopOfStackPointer]
-#         TopOfStackPointer ← TopOfStackPointer – 1
+#         Item ← Stack[top]
+#         top ← top – 1
 #     ENDIF
 #     RETURN Item
 # ENDFUNCTION
