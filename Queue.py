8c659812f855dae2de88dc2279b9ecb2a950fb5b
Muzammil Ahmed <muzidxb1@gmail.com>
2022-03-06 17:54:38 +0000

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,36 +1,81 @@
 size = int(input("Enter size of Queue: "))
 queue = [None for i in range(size)]  # make empty stack with size of input
-front = 0  # front pointer
-end = -1   # rear pointer
+frontp = 0  # frontp pointer
+endp = -1   # rear pointer
 qlen = 0   # Queue length
 
 # UNCOMMENT FOR CIRCULAR QUEUE
 
 def enqueue(value):
-    global end, qlen
+    global endp, qlen
     # if qlen < size:
-        if end < size - 1:
-            end += 1
+        if endp < size - 1:
+            endp += 1
         # else:
-        #     end = 0
-            queue[end] = value  # remove an indent for circular
+        #     endp = 0
+            queue[endp] = value  # remove an indent for circular
             qlen += 1  # remove an indent for circular
-
     else:
         print("Queue full, cannot add")
 
 
 def dequeue():
-    global front, qlen
+    global frontp, qlen
     if qlen == 0:
         print("Queue empty, cannot remove")
     else:
-        queue[front] = None
-        # if front == size - 1:
-        #     front = 0
+        queue[frontp] = None
+        # if frontp == size - 1:
+        #     frontp = 0
         # else:
-        front += 1  # add an indent for circular
+        frontp += 1  # add an indent for circular
         qlen -= 1
+
+# CONSTANT nullp = -1
+# CONSTANT size = 8
+# DECLARE frontp : INTEGER
+# DECLARE endp : INTEGER
+# DECLARE qlen : INTEGER
+# DECLARE Queue : ARRAY[0 : size – 1] OF STRING
+# PROCEDURE InitialiseQueue
+#     frontp ← nullp
+#     endp ← nullp
+#     qlen ← 0
+# ENDPROCEDURE
+
+# PROCEDURE enqueue(value)
+#     IF qlen < size
+#       THEN
+#         endp ← endp + 1
+#         IF endp > size – 1
+#           THEN
+#             endp ← 0
+#         ENDIF
+#         Queue[endp] ← value
+#         qlen ← qlen + 1
+#     ENDIF
+# ENDPROCEDURE
+
+# FUNCTION dequeue()
+#     DECLARE Item : STRING
+#     Item ← ""
+#     IF qlen > 0
+#       THEN
+#         Item ← Queue[frontp]
+#         qlen ← qlen – 1
+#         IF qlen = 0
+#           THEN
+#             CALL InitialiseQueue
+#           ELSE
+#             frontp ← frontp + 1
+#             IF frontp > size – 1
+#               THEN
+#               frontp ← 0
+#             ENDIF
+#         ENDIF
+#     ENDIF
+#     RETURN Item
+# ENDFUNCTION
 
 # uses FIFO
 
